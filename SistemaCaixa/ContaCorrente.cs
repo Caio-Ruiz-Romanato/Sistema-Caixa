b7bcf6308622602ada8e0506473b74cfe90a76b5
Caio Romanato Ruiz - Cadis - Tecnologia <caio.ruiz@cadis.com.br>
2022-06-29 09:31:35 -0300

    SistemaCaixa

---
+++ SistemaCaixa/ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaCaixa
+{
+    internal class ContaCorrente
+    {
+        //Propriedades altoimplementares
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //Construtores
+        public ContaCorrente(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+        public ContaCorrente(int numeroConta, string titular, double depositoInicial) : this(numeroConta, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        //Atributos
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 7.50;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo R$: "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+}
---
+++ SistemaCaixa/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaCaixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta;
+
+            Console.WriteLine("=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            Console.WriteLine("==========   B A N C O N I X   ===========");
+            Console.WriteLine("=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Bem vindo ao banco Nix");
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Informe o nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n) ?");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Haverá depósito inicial ? ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaCorrente(numeroConta, titular, depositoInicial);
+
+            }
+            else
+            {
+                conta = new ContaCorrente(numeroConta, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Qual o valor de depósito R$: ");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Qual o valor para saque R$: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
